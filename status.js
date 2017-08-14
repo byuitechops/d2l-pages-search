1bac347e5bd27f54e58a408c0a4e59f9383e80cd
Xacheiree <wil11063@byui.edu>
2017-08-14 12:29:09 -0600

    Add files via upload

---
+++ status.js
@@ -0,0 +1,14 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['status'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "<div class=\"flex course\" id=\""
+    + alias4(((helper = (helper = helpers.ou || (depth0 != null ? depth0.ou : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ou","hash":{},"data":data}) : helper)))
+    + "-OU\">\r\n  <span class=\"course-title\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span>\r\n  <span class=\"spacer\"></span>\r\n  <span class=\"status-loading\">"
+    + alias4(((helper = (helper = helpers.status || (depth0 != null ? depth0.status : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"status","hash":{},"data":data}) : helper)))
+    + "</span>\r\n</div>\r\n";
+},"useData":true});
+})();
