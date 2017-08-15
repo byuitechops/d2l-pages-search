4a2a7697d309963406419632ff0ae53bd094510d
Zachary Williams <wil11063@byui.edu>
2017-08-15 11:47:43 -0600

    Adjusted text match again

--- js/textMatch.js
+++ js/textMatch.js
@@ -3,12 +3,12 @@
 templates['textMatch'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"result\">\r\n  <span>"
+  return "<div class=\"result\">\r\n  <span>\r\n    "
     + alias4(((helper = (helper = helpers.firstFifty || (depth0 != null ? depth0.firstFifty : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"firstFifty","hash":{},"data":data}) : helper)))
-    + "</span>\r\n  <span class=\"highlight\">\r\n    "
+    + "\r\n    <span class=\"highlight\">\r\n      "
     + ((stack1 = ((helper = (helper = helpers.queryMatch || (depth0 != null ? depth0.queryMatch : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"queryMatch","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "\r\n  </span>\r\n  <span>"
+    + "\r\n    </span>\r\n    "
     + alias4(((helper = (helper = helpers.secondFifty || (depth0 != null ? depth0.secondFifty : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"secondFifty","hash":{},"data":data}) : helper)))
-    + "</span>\r\n</div>\r\n";
+    + "\r\n</span>\r\n</div>\r\n";
 },"useData":true});
 })();
