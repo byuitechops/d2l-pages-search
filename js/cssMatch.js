cca0180729a2d2e814d7660a7f817c04191fef48
scott56gps <scott93gps@gmail.com>
2017-08-25 14:13:05 -0600

    Removed old Handlebars templates and updated the HTML

--- js/cssMatch.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['cssMatch'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"result\">\r\n    <span>\r\n        "
-    + container.escapeExpression(container.lambda(depth0, depth0))
-    + "\r\n    </span>\r\n</div>\r\n";
-},"useData":true});
-})();
